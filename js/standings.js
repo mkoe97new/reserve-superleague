68334755eff530c3466f0d91cf4f09cd9e661b63
mkoe9 <mkoe97@gmail.com>
2026-09-20 23:01:00 +0200

    test games

--- js/standings.js
+++ js/standings.js
@@ -140,7 +140,7 @@
 	}
 
 	function loadGames() {
-		fetch('data/games.json')
+		fetch('data/games2.json')
 			.then(function (response) {
 				if (!response.ok) {
 					throw new Error('Failed to load games');
