a5fadc4d849063885675cdaa1750409dcc4b16cd
mkoe9 <mkoe97@gmail.com>
2026-09-20 19:21:12 +0200

    center text

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "October 6, 2026 12:00:00",
+	date: "November 6, 2026 12:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
