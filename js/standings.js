a81a2ffdd38f39f4cbcd89e81f152db678b6199c
mkoe9 <mkoe97@gmail.com>
2026-09-26 20:12:10 +0200

    real_data

--- js/standings.js
+++ js/standings.js
@@ -204,7 +204,7 @@
 	}
 
 	function loadGames() {
-		fetch('data/games2.json')
+		fetch('data/games.json')
 			.then(function (response) {
 				if (!response.ok) {
 					throw new Error('Failed to load games');
